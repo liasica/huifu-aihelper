bdd837916da31f728ec4a72cc0ce6265e6abea46
liasica <magicrolan@qq.com>
2025-08-21 18:14:38 +0800

    项目初始化

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+
+createApp(App).mount(
+  (() => {
+    const app = document.createElement('div');
+    let interval = setInterval(() => {
+      if (document && document.body) {
+        clearInterval(interval);
+        document.body.append(app);
+      }
+    }, 1);
+    return app;
+  })(),
+);
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import monkey, { cdn } from 'vite-plugin-monkey';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    monkey({
+      entry: 'src/main.ts',
+      userscript: {
+        icon: 'https://www.google.com/s2/favicons?sz=64&domain=huifu.com',
+        namespace: 'https://liasica.com/huifu-aihelper',
+        match: ['https://paas.huifu.com/open/doc/api/*'],
+        version: '1.0.0',
+        description: '汇付天下斗拱平台文档AI助手',
+        author: 'liasica',
+        name: '汇付天下斗拱平台文档AI助手',
+        'run-at': 'document-start',
+      },
+      build: {
+        externalGlobals: {
+          vue: cdn.jsdelivr('Vue', 'dist/vue.global.prod.js'),
+        },
+      },
+    }),
+  ],
+});
