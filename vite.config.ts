f07cb0c795baebb051f554b59f0c1a656134ae1d
liasica <magicrolan@qq.com>
2025-08-25 12:48:42 +0800

    feat: 版本使用环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,41 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import monkey, { cdn } from 'vite-plugin-monkey'
 import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    UnoCSS(),
-    monkey({
-      entry: 'src/main.ts',
-      userscript: {
-        icon: 'https://www.google.com/s2/favicons?sz=64&domain=huifu.com',
-        namespace: 'https://liasica.com/huifu-aihelper',
-        match: ['https://paas.huifu.com/open/doc/api/*'],
-        version: '1.0.0',
-        description: '汇付天下斗拱平台文档AI助手',
-        author: 'liasica',
-        name: '汇付天下斗拱平台文档AI助手',
-        'run-at': 'document-start',
-      },
-      build: {
-        externalGlobals: {
-          vue: cdn.jsdelivr('Vue', 'dist/vue.global.prod.js'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      vue(),
+      UnoCSS(),
+      monkey({
+        entry: 'src/main.ts',
+        userscript: {
+          icon: 'https://www.google.com/s2/favicons?sz=64&domain=huifu.com',
+          namespace: 'https://github.com/liasica/huifu-aihelper',
+          match: ['https://paas.huifu.com/open/doc/api/*'],
+          version: env.VERSION || '1.0.0',
+          description: '汇付天下斗拱平台文档AI助手',
+          author: 'liasica',
+          name: '汇付天下斗拱平台文档AI助手',
+          'run-at': 'document-start',
         },
+        build: {
+          externalGlobals: {
+            vue: cdn.jsdelivr('Vue', 'dist/vue.global.prod.js'),
+          },
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  },
+  }
 })
